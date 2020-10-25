2848462e9bba54fb02c5199c8b0fccb94666240a
Vladislav <vladislavluksha99@gmail.com>
2020-10-25 18:36:07 +0300

    feat: implementation of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++)
+      brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+
+  for (let j = 0; j < brackets.length; j++) {
+      if (str.includes(brackets[j])) {
+          str = str.replace(brackets[j], '');
+          j = -1;
+      } 
+  };
+  return str === '';
 }
